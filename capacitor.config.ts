f39316f7ae541597f0e2c34e1e4e40ba2caa56c6
sophialiu2008 <liulijun1978@hotmail.com>
2026-01-05 22:46:03 +0800

    描述你改了什么

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: '慧相---ai面相智能分析',
+  webDir: 'dist'
+};
+
+export default config;
